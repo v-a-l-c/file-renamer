d466fc5b68a2f145952bbbf74d92d4239daf8917
Val <val.cb@ciencias.unam.mx>
2025-07-30 11:42:42 -0600

    Reading from args

--- Model/Reader.cs
+++ Model/Reader.cs
@@ -7,8 +7,28 @@ namespace Reader
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Holaaa, bienvenide.");
+            Console.WriteLine($"Changing the names of {args[0]}");
         }
 
+        void RecorreDirectorio(string path)
+        {
+            DirectoryInfo d = new DirectoryInfo(path);
+
+            if (!d.Exists)
+            {
+                throw new Exception();
+            }
+
+            FileInfo[] files = d.GetFiles("*.mp3");
+
+            foreach (FileInfo file in files)
+            {
+                //ProcessFile(file.FullName);
+            }
+
+            string[] subDirectorios = Directory.GetDirectories(path);
+            foreach (string subdirectory in subDirectorios)
+                RecorreDirectorio(subdirectory);
+        }
     }
 }
