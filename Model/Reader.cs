110c5edecd4d00852e7e58bddfac1b0679177b31
Val <val.cb@ciencias.unam.mx>
2025-07-30 11:30:00 -0600

    Model

---
+++ Model/Reader.cs
@@ -0,0 +1,14 @@
+using System;
+using System.IO;
+
+namespace Reader
+{
+    class Read
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Holaaa, bienvenide.");
+        }
+
+    }
+}
